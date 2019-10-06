e9ee77fecae945b4a2cc71d6f4335e7d8fe13819
Michael Aldrich <11843596+MichaelAldrich@users.noreply.github.com>
2019-10-05 22:52:03 -0400

    leftovers

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,15 +1,17 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item;
+import net.minecraft.item.tem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ExampleMod implements ModInitializer {
-	
-	public static final Item FABRIC_ITEM = new Item(new Item.Settings().itemGroup(ItemGroup.MISC));
-	
+
+	public static final Item dry_cereal_item = new Item(new Item.Settings().itemGroup(ItemGroup.MISC));
+	public static final Item cereal_bowl_item = new Item(new Item.Settings().itemGroup(ItemGroup.MISC));
+	public static final Item milk_and_cereal_item = new Item(new Item.Settings().itemGroup(ItemGroup.FOOD));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -18,6 +20,8 @@ public class ExampleMod implements ModInitializer {
 
 		System.out.println("Hello Fabric world!");
 
-		Registry.register(Registry.ITEM, new Identifier("cerealcraft", "hi_monica"), FABRIC_ITEM);
+		Registry.register(Registry.ITEM, new Identifier("cerealcraft", "dry_cereal"), dry_cereal_item);
+		Registry.register(Registry.ITEM, new Identifier("cerealcraft", "cereal_bowl"), cereal_bowl_item);
+		Registry.register(Registry.ITEM, new Identifier("cerealcraft", "milk_and_cereal"), milk_and_cereal_item);
 	}
 }
